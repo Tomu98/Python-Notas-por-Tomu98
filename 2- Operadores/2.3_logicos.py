d321ef01ef6f4cf6e814bdc5bc37380bc80770b2
Abel Tomás <131022656+Tomu98@users.noreply.github.com>
2023-11-14 15:49:09 -0300

    Update 2.3_logicos.py
    Edité los "Devolver" por "Devuelve"

--- 2- Operadores/2.3_logicos.py
+++ 2- Operadores/2.3_logicos.py
@@ -3,23 +3,23 @@
 # Estos operadores comparan datos para devolver un dato booleano
 
 # and (&): Devuelve "True" si ambas expresiones son verdaderas y "False" en cualquier otro caso.
-and1 = True & True    # Devolver True.
-and2 = False & True   # Devolver False.
-and3 = True & False   # Devolver False.
-and4 = False & False  # Devolver False.
+and1 = True & True    # Devuelve True.
+and2 = False & True   # Devuelve False.
+and3 = True & False   # Devuelve False.
+and4 = False & False  # Devuelve False.
 
 
 # or (|): Devuelve "False" si ambas condiciones no se cumplen y "True" en cualquier otro caso.
-or1 = True | True    # Devolver True.
-or2 = False | True   # Devolver True.
-or3 = True | False   # Devolver True.
-or4 = False | False  # Devolver False.
+or1 = True | True    # Devuelve True.
+or2 = False | True   # Devuelve True.
+or3 = True | False   # Devuelve True.
+or4 = False | False  # Devuelve False.
 
 
 # not: Devuelve el valor opuesto a la expresión dada.
 # Si la expresión es verdadera, devuelve "False", y si la expresión es falsa, devuelve "True".
-not1 = not True   # Devolver False.
-not2 = not False  # Devolver True.
+not1 = not True   # Devuelve False.
+not2 = not False  # Devuelve True.
 
 
 # Ejemplo:
