d062a9240ad0618448d8c6111818c541014f4b34
Abel Tomás <abeltomasr98@gmail.com>
2023-12-16 00:54:30 -0300

    Correcciones en '7.3_mi_excepcion'

--- 7- Excepciones/7.3_mi_excepcion.py
+++ 7- Excepciones/7.3_mi_excepcion.py
@@ -1,19 +1,17 @@
-# --- MIS EXCEPCIONES ---
+# MIS EXCEPCIONES
 
-
-# Si bien Python proporciona muchas excepciones predefinidas, a veces es útil crear tus propias excepciones personalizadas para manejar situaciones específicas de tu programa de manera más efectiva.
+# Python proporciona muchas excepciones predefinidas, pero a veces es útil crear tus propias excepciones personalizadas para manejar situaciones específicas de tu programa de manera más efectiva.
 # Puedes definir excepciones personalizadas para reflejar problemas o situaciones únicas en tu aplicación.
 
 
 # "raise"
 # La palabra clave "raise" se utiliza para generar una excepción manualmente en Python.
 # Permite que el programador genere una excepción específica cuando se cumple cierta condición.
-# La sintaxis básica de "raise" es:
+# Su sintaxis sería:
 # "raise Exception("Mensaje de error")"
-
-# - "raise": La palabra clave utilizada para levantar una excepción.
-# - "Exception": El tipo de excepción que se está creando. En este caso, estamos utilizando la clase base "Exception".
-# - "Mensaje de error": Un mensaje opcional que proporciona información adicional sobre la excepción.
+# - "raise": la palabra clave utilizada para levantar una excepción.
+# - "Exception": el tipo de excepción que se está creando, en este caso, estamos utilizando la clase base "Exception".
+# - "Mensaje de error": un mensaje opcional que proporciona información adicional sobre la excepción.
 
 
 # Clase "Exception"
@@ -21,34 +19,36 @@
 # Esto significa que puedes crear tus propias excepciones personalizadas extendiendo la clase "Exception".
 # La estructura básica para definir una excepción personalizada es:
 class MiExcepcionEjemplo(Exception):
+    """Subclase de 'Exception' como ejemplo"""
     def __init__(self, mensaje):
         self.mensaje = mensaje
         super().__init__(self.mensaje)
 
-# - "MiExcepcion": El nombre de la excepción personalizada que estás creando.
-# - "Exception": Indica que tu excepción personalizada es una subclase de "Exception".
-# - "__init__(self, mensaje)": El método constructor de la excepción personalizada, que toma un argumento "mensaje" para proporcionar información adicional sobre la excepción.
-# - super().__init__(self.mensaje): Llama al constructor de la clase base Exception y pasa el mensaje como argumento para inicializar correctamente la excepción.
+# - "MiExcepcionEjemplo": el nombre de la excepción personalizada que estás creando.
+# - "Exception": indica que tu excepción personalizada es una subclase de "Exception".
+# - "__init__(self, mensaje)": método constructor de la excepción personalizada, que toma un argumento "mensaje" para proporcionar información adicional sobre la excepción.
+# - super().__init__(self.mensaje): llama al constructor de la clase base Exception y pasa el mensaje como argumento para inicializar correctamente la excepción.
 
 
-# Ejemplo:
-# Definir una excepción personalizada
+# Ejemplo definiendo una excepción personalizada
 class MiExcepcion(Exception):
+    """Clase de ejemplo"""
     def __init__(self, mensaje):
         self.mensaje = mensaje
         super().__init__(self.mensaje)
 
 # Función que simula una operación que podría generar la excepción personalizada
 def dividir(a, b):
+    """Función de dividir"""
     if b == 0:
         raise MiExcepcion("No es posible dividir por cero")
     return a / b
 
 # Uso de la excepción personalizada
 try:
-    numerador = 10
-    divisor = 0
-    resultado = dividir(numerador, divisor)
+    NUMERADOR = 10
+    DIVISOR = 0
+    resultado = dividir(NUMERADOR, DIVISOR)
 except MiExcepcion as e:
     print(f"Se ha producido una excepción personalizada: {e}")
 else:
@@ -62,7 +62,3 @@ else:
 # En el bloque "try"..."except", intentamos realizar una división con un divisor igual a cero, lo que debería generar nuestra excepción personalizada.
 # Si se levanta la excepción, capturamos la instancia de la excepción en la variable "e" y mostramos el mensaje personalizado.
 # Si no se levanta ninguna excepción, es decir, si la división se realiza con éxito, imprimimos el resultado.
-
-
-# Este ejemplo ilustra cómo crear, levantar y manejar una excepción personalizada en Python.
-# La excepción "MiExcepcion" nos permite comunicar de manera efectiva un problema específico (división por cero en este caso) y controlar cómo se maneja este error en nuestro código.
